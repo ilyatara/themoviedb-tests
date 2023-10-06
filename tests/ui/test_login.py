d1f0e8fb4b9ccbfc8b46417a40daa85d2962e2de
Ilya Tarasov <qailyatarasov@gmail.com>
2023-10-06 23:11:00 +0600

    Added UI tests for login page

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,20 +1,32 @@
 from selene import browser, be, have
 
-import project
 
-
-def test_open_login_page():
+def test_login_with_invalid_password():
     # ACT
     browser.open('/login')
+    browser.element('#username').type(project.config.tmdb_login)
+    browser.element('#password').type(project.config.tmdb_password[::-1])
+    browser.element('#login_button').click()
     # ASSERT
-    browser.element('.wrapper > h2').should(have.exact_text('Login to your account'))
+    browser.element('.error_status').should(be.visible)
+    browser.all('.error_status a').should(have.text('There was a problem'))
+    browser.all('.error_status li').should(have.size(2))
 
 
-def test_login_successful():
+def test_login_with_empty_username():
     # ACT
     browser.open('/login')
-    if browser.element('#onetrust-accept-btn-handler').matching(be.present):
-        browser.element('#onetrust-accept-btn-handler').click()
+    browser.element('#password').type(project.config.tmdb_password)
+    browser.element('#login_button').click()
+    # ASSERT
+    browser.element('.error_status').should(be.visible)
+    browser.all('.error_status a').should(have.text('There was a problem'))
+    browser.all('.error_status li').should(have.size(2))
+
+
+def test_login_with_valid_username_and_password():
+    # ACT
+    browser.open('/login')
     browser.element('#username').type(project.config.tmdb_login)
     browser.element('#password').type(project.config.tmdb_password)
     browser.element('#login_button').click()
@@ -22,8 +34,28 @@ def test_login_successful():
     browser.element(f'li.user a[href="/u/{project.config.tmdb_login}"]').should(be.visible)
 
 
-def test_auth_cookie(logged_in):
+
+def test_login_page_contents():
     # ACT
-    browser.open(f'/u/{project.config.tmdb_login}')
+    browser.open('/login')
     # ASSERT
-    browser.element(f'li.user a[href="/u/{project.config.tmdb_login}"]').should(be.present)
+    browser.element('.wrapper h2').should(be.visible)
+    browser.element('#username').should(be.visible)
+    browser.element('#password').should(be.visible)
+    browser.element('#login_button').should(be.visible)
+    browser.element('.reset a').should(be.visible)
+    browser.element('.error_status').should(be.not_.visible)
+
+
+# def test_open_login_page():
+#     # ACT
+#     browser.open('/login')
+#     # ASSERT
+#     browser.element('.wrapper > h2').should(have.exact_text('Login to your account'))
+#
+#
+# def test_auth_cookie(logged_in):
+#     # ACT
+#     browser.open(f'/u/{project.config.tmdb_login}')
+#     # ASSERT
+#     browser.element(f'li.user a[href="/u/{project.config.tmdb_login}"]').should(be.present)
