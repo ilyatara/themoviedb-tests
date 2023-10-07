1e88ddd1474b78fe257be1a3590569349cbd6717
Ilya Tarasov <qailyatarasov@gmail.com>
2023-10-07 20:20:15 +0600

    Added tests for search hints

--- tests/ui/test_search.py
+++ tests/ui/test_search.py
@@ -18,3 +18,18 @@ def test_select_search_tabs_with_no_results():
     search_tabs_reversed = list([st for st in SearchTabs])[::-1]
     for st in search_tabs_reversed:
         page.should_have_search_tab_active_with_text(st)
+
+
+def test_search_hints_are_visible_after_click_on_search_input():
+    page = SearchPage()
+    page.open()
+    page.click_on_search_input()
+    page.should_have_search_hints_visible()
+
+
+def test_search_hins_are_functional():
+    page = SearchPage()
+    page.open()
+    page.click_on_search_input()
+    page.click_on_search_hint()
+    page.should_have_movie_search_result()
--- themoviedb_tests/pages/search_page.py
+++ themoviedb_tests/pages/search_page.py
@@ -1,4 +1,4 @@
-from selene import browser, be, have
+from selene import browser, be, have, query
 
 from themoviedb_tests import utils
 from themoviedb_tests.data.search_tabs import SearchTabs
@@ -14,10 +14,27 @@ class SearchPage:
     search_menu_tabs_counters = browser.all('#search_menu_scroller span')
     search_results_container = browser.element('.white_column')
     search_hints_container = browser.element('.k-animation-container')
+    search_hints = search_hints_container.all('li')
+    movie_search_results = search_results_container.all('div[id^=card_movie_')
 
     def open(self):
         browser.open(self.url)
         utils.ui.close_cookies_banner()
+
+    def click_on_search_input(self):
+        self.search_input.click()
+
+    def click_on_search_hint(self, index=4):
+        self.search_request = self.search_hints[index].get(query.text)
+        self.search_hints[index].click()
+
+    def should_have_movie_search_result(self):
+        self.movie_search_results[0].element('h2').should(have.exact_text(self.search_request))
+        self.search_request = None
+
+    def should_have_search_hints_visible(self):
+        self.search_hints_container.should(be.visible)
+        self.search_hints.should(have.size(10))
 
     def should_have_contents_visible(self):
         self.search_input.should(be.visible)
@@ -38,11 +55,3 @@ class SearchPage:
         self.search_results_container.element('.search_results:not(.hide)').should(
             have.exact_text(search_tab.get_no_results_text())
         )
-
-    # def should_have_search_tabs_active_with_texts(self):
-    #     for st in SearchTabs:
-    #         browser.element(st.css_id).click()
-    #         browser.element(st.css_id).should(have.css_class(('active')))
-    #         self.search_results_container.element('.search_results:not(.hide)').should(
-    #             have.exact_text(st.get_no_results_text())
-    #         )
