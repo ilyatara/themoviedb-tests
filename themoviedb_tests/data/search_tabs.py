afaf9e6c3be07b40ff8e80f632b932d52755838b
Ilya Tarasov <qailyatarasov@gmail.com>
2023-10-07 16:46:16 +0600

    Store search tabs in Enum class

---
+++ themoviedb_tests/data/search_tabs.py
@@ -0,0 +1,15 @@
+import enum
+
+
+class SearchTabs(enum.Enum):
+    movies = {'css_id': '#movie', 'tab_name': 'Movies'}
+    tv_shows = {'css_id': '#tv', 'tab_name': 'TV Shows'}
+    people = {'css_id': '#person', 'tab_name': 'People'}
+    collections = {'css_id': '#collection', 'tab_name': 'Collections'}
+    companies = {'css_id': '#company', 'tab_name': 'Companies'}
+    keywords = {'css_id': '#keyword', 'tab_name': 'Keywords'}
+    networks = {'css_id': '#network', 'tab_name': 'Networks'}
+
+    def __init__(self, vals):
+        self.css_id = vals['css_id']
+        self.tab_name = vals['tab_name']
--- themoviedb_tests/pages/search_page.py
+++ themoviedb_tests/pages/search_page.py
@@ -1,17 +1,17 @@
 from selene import browser, be, have
 
 from themoviedb_tests import utils
+from themoviedb_tests.data.search_tabs import SearchTabs
 
 
 class SearchPage:
     url = '/search'
     search_input = browser.element('#search_v4')
     search_menu = browser.element('.grey_column')
-    search_menu_tabs_ids = (
-        '#movie, #tv, #person, #collection, #company, #keyword, #network'
-    )
+    search_menu_tabs_ids = ', '.join([st.css_id for st in SearchTabs])
     search_menu_tabs = browser.all(search_menu_tabs_ids)
-    searchh_menu_tabs_count = len(search_menu_tabs_ids.split(','))
+    search_menu_tabs_count = len(SearchTabs)
+    search_menu_tabs_counters = browser.all('#search_menu_scroller span')
     search_results_container = browser.element('.white_column')
 
     def open(self):
@@ -19,13 +19,21 @@ class SearchPage:
         utils.ui.close_cookies_banner()
 
     def should_have_contents_visible(self):
+        # TODO: check that search suggestions are not available
         self.search_input.should(be.visible)
         self.search_menu.should(be.visible)
         self.search_results_container.should(be.visible)
-        self.search_menu_tabs.should(have.size(self.searchh_menu_tabs_count))
+        self.search_menu_tabs.should(
+            have.size(self.search_menu_tabs_count)
+        )
+        self.search_menu_tabs_counters.should(
+            have.exact_texts(['0'] * self.search_menu_tabs_count)
+        )
+        # TODO: move to another test
         browser.element('#movie').should(have.css_class(('active')))
         browser.element('.search_results .movie').should(
             have.exact_text('There are no movies that matched your query.')
         )
 
-
+    def should_have_menu_tabs_active_with_texts(self):
+        pass
